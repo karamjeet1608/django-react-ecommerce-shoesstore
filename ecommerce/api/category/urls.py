14c5b2ffb817d76674dcb9b8585a3f55c6466b48
karamjeetsingh.coderK <karamjeetsingh.de@gmail.com>
2022-05-04 14:17:13 +0200

    added category and products

--- ecommerce/api/category/urls.py
+++ ecommerce/api/category/urls.py
@@ -1,8 +1,11 @@
+from rest_framework import routers
 from django.urls import path, include
-from rest_framework.authtoken import views
 
+from . import views
+
+router = routers.DefaultRouter()
+router.register(r'', views.CategoryViewSet)
 
 urlpatterns = [
-   
-
+    path('', include(router.urls))
 ]
--- ecommerce/api/product/urls.py
+++ ecommerce/api/product/urls.py
@@ -1,8 +1,11 @@
+from rest_framework import routers
 from django.urls import path, include
-from rest_framework.authtoken import views
 
+from . import views
+
+router = routers.DefaultRouter()
+router.register(r'', views.ProductViewSet)
 
 urlpatterns = [
-   
-
+    path('', include(router.urls))
 ]
